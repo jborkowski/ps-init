b36c145de8f8069fe102b6d514d0659c401964f9
Jonatan Borkowski <jonatan.borkowski@pm.me>
2020-11-11 23:13:21 +0100

    Example with Effect and Aff

--- src/Main.purs
+++ src/Main.purs
@@ -2,15 +2,32 @@ module Main where
 
 import Prelude
 
-import Data.Maybe (Maybe(..))
+import Affjax as AX
+import Affjax.ResponseFormat as AXRF
+import Data.Either (hush)
+import Data.Maybe (Maybe(..), maybe)
 import Effect (Effect)
+import Effect.Aff.Class (class MonadAff)
+import Effect.Class (class MonadEffect)
+import Effect.Random (random)
+import Halogen (HalogenQ(..))
 import Halogen as H
 import Halogen.Aff as HA
+import Halogen.HTML (output)
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
+import Halogen.HTML.Properties as HP
 import Halogen.VDom.Driver (runUI)
+import Web.Event.Event (Event)
+import Web.Event.Event as Event
 
+main :: Effect Unit
+main = HA.runHalogenAff do
+  body <- HA.awaitBody
+  runUI component unit body
 
+-- | First part - Without effect
+{-
 data Action = Increment | Decrement
 
 type Input = Unit
@@ -42,9 +59,104 @@ component =
 
       Increment ->
         H.modify_ \state -> state + 1
+-}
 
+-- | With Effect - Random
+{-
+type State = Maybe Number
 
-main :: Effect Unit
-main = HA.runHalogenAff do
-  body <- HA.awaitBody
-  runUI component unit body
+data Action = Regenerate
+
+component :: forall query input output m. MonadEffect m => H.Component HH.HTML query input output m
+component =
+  H.mkComponent
+    { initialState
+    , render
+    , eval: H.mkEval $ H.defaultEval { handleAction = handleAction }
+    }
+
+initialState :: forall input. input -> State
+initialState _ = Nothing
+
+render :: forall m. State -> H.ComponentHTML Action () m
+render state = do
+  let value = maybe "No number generated yet" show state
+  HH.div_
+    [ HH.h1_
+      [ HH.text "Random numer" ]
+    , HH.p_
+      [ HH.text ("Current value: " <> value) ]
+    , HH.button
+      [ HE.onClick \_ -> Just Regenerate ]
+      [ HH.text "Generate new number" ]
+    ]
+
+handleAction :: forall output m. MonadEffect m => Action -> H.HalogenM State Action () output m Unit
+handleAction = case _ of
+  Regenerate -> do
+    newNumber <- H.liftEffect random -- lift Effect to MonadEffect
+    H.modify_ \_ -> Just newNumber
+-}
+
+type State =
+  { loading :: Boolean
+  , username :: String
+  , result :: Maybe String
+  }
+
+data Action =
+    SetUsername String
+  | MakeRequest Event
+
+component :: forall query input output m. MonadAff m => H.Component HH.HTML query input output m
+component =
+  H.mkComponent
+    { initialState
+    , render
+    , eval: H.mkEval $ H.defaultEval { handleAction = handleAction }
+    }
+
+initialState :: forall i. i -> State
+initialState _ = { loading: false, username: "", result: Nothing }
+
+render :: forall m. State -> H.ComponentHTML Action () m
+render st =
+  HH.form
+    [ HE.onSubmit \ev -> Just (MakeRequest ev) ]
+    [ HH.h1_ [ HH.text "Look up GitHub user" ]
+    , HH.label_
+        [ HH.div_ [ HH.text "Enter username:" ]
+        , HH.input
+            [ HP.value st.username
+            , HE.onValueInput \str -> Just (SetUsername str)
+            ]
+        ]
+    , HH.button
+        [ HP.disabled st.loading
+        , HP.type_ HP.ButtonSubmit
+        ]
+        [ HH.text "Fetch info" ]
+    , HH.p_
+        [ HH.text (if st.loading then "Working..." else "") ]
+    , HH.div_
+        case st.result of
+          Nothing -> []
+          Just res ->
+            [ HH.h2_
+                [ HH.text "Response:" ]
+            , HH.pre_
+                [ HH.code_ [ HH.text res ] ]
+            ]
+    ]
+
+handleAction :: forall output m. MonadAff m => Action -> H.HalogenM State Action () output m Unit
+handleAction = case _ of
+  SetUsername username -> do
+    H.modify_ _ { username = username, result = Nothing }
+
+  MakeRequest event -> do
+    H.liftEffect $ Event.preventDefault event
+    username <- H.gets _.username
+    H.modify_ _ { loading = true }
+    response <- H.liftAff $ AX.get AXRF.string ("https://api.github.com/users/" <> username)
+    H.modify_ _ { loading = false, result = map _.body (hush response) }
