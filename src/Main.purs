5b83a46081ccdec84afb223fd7acbcb2ca3d2366
Jonatan Borkowski <jonatan.borkowski@pm.me>
2020-11-13 23:35:05 +0100

    Add Parent component - part 1

--- src/Main.purs
+++ src/Main.purs
@@ -3,9 +3,12 @@ module Main where
 import Prelude
 
 import Affjax as AX
+import Affjax.RequestBody (RequestBody(..))
 import Affjax.ResponseFormat as AXRF
+import Control.Category (identity)
 import Data.Either (hush)
 import Data.Maybe (Maybe(..), maybe)
+import Data.Symbol (SProxy(..))
 import Effect (Effect)
 import Effect.Aff.Class (class MonadAff)
 import Effect.Class (class MonadEffect)
@@ -13,18 +16,19 @@ import Effect.Random (random)
 import Halogen (HalogenQ(..))
 import Halogen as H
 import Halogen.Aff as HA
-import Halogen.HTML (output)
+import Halogen.HTML (input, label, output, slot)
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
 import Halogen.VDom.Driver (runUI)
+import PSCI.Support (eval)
 import Web.Event.Event (Event)
 import Web.Event.Event as Event
 
 main :: Effect Unit
 main = HA.runHalogenAff do
   body <- HA.awaitBody
-  runUI component unit body
+  runUI parent unit body
 
 -- | First part - Without effect
 {-
@@ -98,65 +102,100 @@ handleAction = case _ of
     H.modify_ \_ -> Just newNumber
 -}
 
-type State =
-  { loading :: Boolean
-  , username :: String
-  , result :: Maybe String
-  }
+-- type State =
+--   { loading :: Boolean
+--   , username :: String
+--   , result :: Maybe String
+--   }
 
-data Action =
-    SetUsername String
-  | MakeRequest Event
+-- data Action =
+--     SetUsername String
+--   | MakeRequest Event
 
-component :: forall query input output m. MonadAff m => H.Component HH.HTML query input output m
-component =
+-- component :: forall query input output m. MonadAff m => H.Component HH.HTML query input output m
+-- component =
+--   H.mkComponent
+--     { initialState
+--     , render
+--     , eval: H.mkEval $ H.defaultEval { handleAction = handleAction }
+--     }
+
+-- initialState :: forall i. i -> State
+-- initialState _ = { loading: false, username: "", result: Nothing }
+
+-- render :: forall m. State -> H.ComponentHTML Action () m
+-- render st =
+--   HH.form
+--     [ HE.onSubmit \ev -> Just (MakeRequest ev) ]
+--     [ HH.h1_ [ HH.text "Look up GitHub user" ]
+--     , HH.label_
+--         [ HH.div_ [ HH.text "Enter username:" ]
+--         , HH.input
+--             [ HP.value st.username
+--             , HE.onValueInput \str -> Just (SetUsername str)
+--             ]
+--         ]
+--     , HH.button
+--         [ HP.disabled st.loading
+--         , HP.type_ HP.ButtonSubmit
+--         ]
+--         [ HH.text "Fetch info" ]
+--     , HH.p_
+--         [ HH.text (if st.loading then "Working..." else "") ]
+--     , HH.div_
+--         case st.result of
+--           Nothing -> []
+--           Just res ->
+--             [ HH.h2_
+--                 [ HH.text "Response:" ]
+--             , HH.pre_
+--                 [ HH.code_ [ HH.text res ] ]
+--             ]
+--     ]
+
+-- handleAction :: forall output m. MonadAff m => Action -> H.HalogenM State Action () output m Unit
+-- handleAction = case _ of
+--   SetUsername username -> do
+--     H.modify_ _ { username = username, result = Nothing }
+
+--   MakeRequest event -> do
+--     H.liftEffect $ Event.preventDefault event
+--     username <- H.gets _.username
+--     H.modify_ _ { loading = true }
+--     response <- H.liftAff $ AX.get AXRF.string ("https://api.github.com/users/" <> username)
+--     H.modify_ _ { loading = false, result = map _.body (hush response) }
+
+type Slots = ( button :: forall query. H.Slot query Void Int )
+_button = SProxy :: SProxy "button"
+
+parent :: forall query input output m. H.Component HH.HTML query input output m
+parent =
+  H.mkComponent
+    { initialState: identity
+    , render
+    , eval: H.mkEval H.defaultEval
+    }
+  where
+    render :: forall state action. state -> H.ComponentHTML action Slots m
+    render _ =
+      HH.div_ [ HH.slot _button 0 button { label: "Click Me" } absurd ]
+
+-- button :: forall w i. { label :: String } -> HH.HTML w i
+-- button { label } = HH.button [ ] [ HH.text label ]
+
+type Input = { label :: String }
+type State = { label :: String }
+
+button :: forall query output m. H.Component HH.HTML query Input output m
+button =
   H.mkComponent
     { initialState
     , render
-    , eval: H.mkEval $ H.defaultEval { handleAction = handleAction }
+    , eval: H.mkEval H.defaultEval
     }
+  where
+    initialState :: Input -> State
+    initialState input = input
 
-initialState :: forall i. i -> State
-initialState _ = { loading: false, username: "", result: Nothing }
-
-render :: forall m. State -> H.ComponentHTML Action () m
-render st =
-  HH.form
-    [ HE.onSubmit \ev -> Just (MakeRequest ev) ]
-    [ HH.h1_ [ HH.text "Look up GitHub user" ]
-    , HH.label_
-        [ HH.div_ [ HH.text "Enter username:" ]
-        , HH.input
-            [ HP.value st.username
-            , HE.onValueInput \str -> Just (SetUsername str)
-            ]
-        ]
-    , HH.button
-        [ HP.disabled st.loading
-        , HP.type_ HP.ButtonSubmit
-        ]
-        [ HH.text "Fetch info" ]
-    , HH.p_
-        [ HH.text (if st.loading then "Working..." else "") ]
-    , HH.div_
-        case st.result of
-          Nothing -> []
-          Just res ->
-            [ HH.h2_
-                [ HH.text "Response:" ]
-            , HH.pre_
-                [ HH.code_ [ HH.text res ] ]
-            ]
-    ]
-
-handleAction :: forall output m. MonadAff m => Action -> H.HalogenM State Action () output m Unit
-handleAction = case _ of
-  SetUsername username -> do
-    H.modify_ _ { username = username, result = Nothing }
-
-  MakeRequest event -> do
-    H.liftEffect $ Event.preventDefault event
-    username <- H.gets _.username
-    H.modify_ _ { loading = true }
-    response <- H.liftAff $ AX.get AXRF.string ("https://api.github.com/users/" <> username)
-    H.modify_ _ { loading = false, result = map _.body (hush response) }
+    render :: forall action. State -> H.ComponentHTML action () m
+    render { label } = HH.button [ ] [ HH.text label ]
