9c506403477eaf3361956480b01f7f3287e80d25
Jonatan Borkowski <jonatan.borkowski@pm.me>
2020-11-14 00:16:50 +0100

    Full example

--- src/Main.purs
+++ src/Main.purs
@@ -2,18 +2,15 @@ module Main where
 
 import Prelude
 
-import Control.Monad.Rec.Class (forever)
 import Data.Maybe (Maybe(..))
 import Data.Symbol (SProxy(..))
-import Data.Time.Duration (Milliseconds(..))
 import Effect (Effect)
-import Effect.Aff as Aff
-import Effect.Aff.Class (class MonadAff)
-import Effect.Exception (error)
+import Effect.Class (class MonadEffect)
+import Effect.Class.Console (logShow)
 import Halogen as H
 import Halogen.Aff as HA
 import Halogen.HTML as HH
-import Halogen.Query.EventSource as EventSource
+import Halogen.HTML.Events as HE
 import Halogen.VDom.Driver (runUI)
 
 main :: Effect Unit
@@ -21,206 +18,156 @@ main = HA.runHalogenAff do
   body <- HA.awaitBody
   runUI parent unit body
 
--- | First part - Without effect
-{-
-data Action = Increment | Decrement
+-- The parent component supports one type of child component, which uses the
+-- `ButtonSlot` slot type. You can have as many of this type of child component
+-- as there are integers.
+type Slots = ( button :: ButtonSlot Int )
 
-type Input = Unit
-type State = Int
+-- The parent component can only evaluate one action: handling output messages
+-- from the button component, of type `ButtonOutput`.
+data ParentAction = HandleButton ButtonOutput
 
-component :: forall query input output m. H.Component HH.HTML query input output m
-component =
-  H.mkComponent
-    { initialState
-    , render
-    , eval: H.mkEval H.defaultEval { handleAction = handleAction }
-    }
-  where
-    initialState :: forall input. input -> State
-    initialState _ = 0
+-- The parent component maintains in local state the number of times all its
+-- child component buttons have been clicked.
+type ParentState = { clicked :: Int }
 
-    render :: forall m. State -> H.ComponentHTML Action () m
-    render state =
-      HH.div_
-        [ HH.button [ HE.onClick \_ -> Just Decrement ] [ HH.text "-" ]
-        , HH.text (show state)
-        , HH.button [ HE.onClick \_ -> Just Increment ] [ HH.text "+" ]
-        ]
-
-    handleAction :: forall output m. Action -> H.HalogenM State Action () output m Unit
-    handleAction = case _ of
-      Decrement ->
-        H.modify_ \state -> state - 1
-
-      Increment ->
-        H.modify_ \state -> state + 1
--}
-
--- | With Effect - Random
-{-
-type State = Maybe Number
-
-data Action = Regenerate
-
-component :: forall query input output m. MonadEffect m => H.Component HH.HTML query input output m
-component =
-  H.mkComponent
-    { initialState
-    , render
-    , eval: H.mkEval $ H.defaultEval { handleAction = handleAction }
-    }
-
-initialState :: forall input. input -> State
-initialState _ = Nothing
-
-render :: forall m. State -> H.ComponentHTML Action () m
-render state = do
-  let value = maybe "No number generated yet" show state
-  HH.div_
-    [ HH.h1_
-      [ HH.text "Random numer" ]
-    , HH.p_
-      [ HH.text ("Current value: " <> value) ]
-    , HH.button
-      [ HE.onClick \_ -> Just Regenerate ]
-      [ HH.text "Generate new number" ]
-    ]
-
-handleAction :: forall output m. MonadEffect m => Action -> H.HalogenM State Action () output m Unit
-handleAction = case _ of
-  Regenerate -> do
-    newNumber <- H.liftEffect random -- lift Effect to MonadEffect
-    H.modify_ \_ -> Just newNumber
--}
-
--- type State =
---   { loading :: Boolean
---   , username :: String
---   , result :: Maybe String
---   }
-
--- data Action =
---     SetUsername String
---   | MakeRequest Event
-
--- component :: forall query input output m. MonadAff m => H.Component HH.HTML query input output m
--- component =
---   H.mkComponent
---     { initialState
---     , render
---     , eval: H.mkEval $ H.defaultEval { handleAction = handleAction }
---     }
-
--- initialState :: forall i. i -> State
--- initialState _ = { loading: false, username: "", result: Nothing }
-
--- render :: forall m. State -> H.ComponentHTML Action () m
--- render st =
---   HH.form
---     [ HE.onSubmit \ev -> Just (MakeRequest ev) ]
---     [ HH.h1_ [ HH.text "Look up GitHub user" ]
---     , HH.label_
---         [ HH.div_ [ HH.text "Enter username:" ]
---         , HH.input
---             [ HP.value st.username
---             , HE.onValueInput \str -> Just (SetUsername str)
---             ]
---         ]
---     , HH.button
---         [ HP.disabled st.loading
---         , HP.type_ HP.ButtonSubmit
---         ]
---         [ HH.text "Fetch info" ]
---     , HH.p_
---         [ HH.text (if st.loading then "Working..." else "") ]
---     , HH.div_
---         case st.result of
---           Nothing -> []
---           Just res ->
---             [ HH.h2_
---                 [ HH.text "Response:" ]
---             , HH.pre_
---                 [ HH.code_ [ HH.text res ] ]
---             ]
---     ]
-
--- handleAction :: forall output m. MonadAff m => Action -> H.HalogenM State Action () output m Unit
--- handleAction = case _ of
---   SetUsername username -> do
---     H.modify_ _ { username = username, result = Nothing }
-
---   MakeRequest event -> do
---     H.liftEffect $ Event.preventDefault event
---     username <- H.gets _.username
---     H.modify_ _ { loading = true }
---     response <- H.liftAff $ AX.get AXRF.string ("https://api.github.com/users/" <> username)
---     H.modify_ _ { loading = false, result = map _.body (hush response) }
-
-type Slots = ( button :: forall query. H.Slot query Void Int )
-_button = SProxy :: SProxy "button"
-
-type ParentState = { count :: Int }
-
-data ParentAction = Initialize | Increment
-
-parent :: forall query input output m. MonadAff m => H.Component HH.HTML query input output m
+-- The parent component uses no query, input, or output types of its own. It can
+-- use any monad so long as that monad can run `Effect` functions.
+parent :: forall query input output m. MonadEffect m => H.Component HH.HTML query input output m
 parent =
   H.mkComponent
     { initialState
     , render
-    , eval: H.mkEval $ H.defaultEval
-      { handleAction = handleAction
-      , initialize = Just Initialize
-      }
+      -- The only internal event this component can handle are actions as
+      -- defined in the `ParentAction` type.
+    , eval: H.mkEval $ H.defaultEval { handleAction = handleAction }
     }
   where
-    initialState :: input -> ParentState
-    initialState _ = { count: 0 }
+  initialState :: input -> ParentState
+  initialState _ = { clicked: 0 }
 
-    render :: ParentState -> H.ComponentHTML ParentAction Slots m
-    render { count } =
-      HH.div_ [ HH.slot _button 0 button { label: show count } absurd ]
+  -- We render three buttons, handling their output messages with the `HandleButton`
+  -- action. When our state changes this render function will run again, each time
+  -- sending new input (which contains a new label for the child button component
+  -- to use.)
+  render :: ParentState -> H.ComponentHTML ParentAction Slots m
+  render { clicked } = do
+    let clicks = show clicked
+    HH.div_
+      [ -- We render our first button with the slot id 0
+        HH.slot _button 0 button { label: clicks <> " Enabled" } (Just <<< HandleButton)
+        -- We render our second button with the slot id 1
+      , HH.slot _button 1 button { label: clicks <> " Power" } (Just <<< HandleButton)
+        -- We render our third button with the slot id 2
+      , HH.slot _button 2 button { label: clicks <> " Switch" } (Just <<< HandleButton)
+      ]
 
-    handleAction :: ParentAction -> H.HalogenM ParentState ParentAction Slots output m Unit
-    handleAction = case _ of
-      Initialize -> do
-        void $ H.subscribe $ EventSource.affEventSource \emitter -> do
-          fiber <- Aff.forkAff $ forever do
-            Aff.delay $ Milliseconds 1000.0
-            EventSource.emit emitter Increment
+  handleAction :: ParentAction -> H.HalogenM ParentState ParentAction Slots output m Unit
+  handleAction = case _ of
+    -- We handle one action, `HandleButton`, which itself handles the output messages
+    -- of our button component.
+    HandleButton output -> case output of
+      -- There is only one output message, `Clicked`.
+      Clicked -> do
+        -- When the `Clicked` message arises we will increment our clicked count
+        -- in state, then send a query to the first button to tell it to be `true`,
+        -- then send a query to all the child components requesting their current
+        -- enabled state, which we log to the console.
+        H.modify_ \state -> state { clicked = state.clicked + 1 }
+        _ <- H.query _button 0 $ H.tell (SetEnabled true)
+        on <- H.queryAll _button $ H.request GetEnabled
+        logShow on
 
-          pure $ EventSource.Finalizer do
-            Aff.killFiber (error " Event source finalized") fiber
+-- We now move on to the child component, a component called `button`.
 
-      Increment ->
-        H.modify_ \st -> st { count = st.count + 1 }
+-- This component can accept queries of type `ButtonQuery` and send output
+-- messages of type `ButtonOutput`. This slot type is exported so that other
+-- components can use it when constructing their row of slots.
+type ButtonSlot = H.Slot ButtonQuery ButtonOutput
 
+-- We think our button will have the label "button" in the row where it's used,
+-- so we're exporting a symbol proxy for convenience.
+_button = SProxy :: SProxy "button"
 
--- button :: forall w i. { label :: String } -> HH.HTML w i
--- button { label } = HH.button [ ] [ HH.text label ]
+-- This component accepts two queries. The first is a request-style query that
+-- lets a parent component request a `Boolean` value from us. The second is a
+-- tell-style query that lets a parent component send a `Boolean` value to us.
+data ButtonQuery a
+  = GetEnabled (Boolean -> a)
+  | SetEnabled Boolean a
 
+-- This component can notify parent components of one event, `Clicked`
+data ButtonOutput
+  = Clicked
+
+-- This component can handle two internal actions. It can evaluate a `Click`
+-- action and it can receive new input when its parent re-renders.
+data ButtonAction
+  = Click
+  | Receive ButtonInput
+
+-- This component accepts a label as input
 type ButtonInput = { label :: String }
-type ButtonState = { label :: String }
-data ButtonAction = Receive ButtonInput
 
-button :: forall query output m. H.Component HH.HTML query ButtonInput output m
+-- This component stores a label and an enabled flag in state
+type ButtonState = { label :: String, enabled :: Boolean }
+
+-- This component supports queries of type `ButtonQuery`, requires input of
+-- type `ButtonInput`, and can send outputs of type `ButtonOutput`. It doesn't
+-- perform any effects, which we can tell because the `m` type parameter has
+-- no constraints.
+button :: forall m. H.Component HH.HTML ButtonQuery ButtonInput ButtonOutput m
 button =
   H.mkComponent
     { initialState
     , render
+      -- This component can handle internal actions, handle queries sent by a
+      -- parent component, and update when it receives new input.
     , eval: H.mkEval $ H.defaultEval
-      { handleAction = handleAction
-      , receive = Just <<< Receive
-      }
+        { handleAction = handleAction
+        , handleQuery = handleQuery
+        , receive = Just <<< Receive
+        }
     }
   where
-    initialState :: ButtonInput -> ButtonState
-    initialState { label } = { label }
+  initialState :: ButtonInput -> ButtonState
+  initialState { label } = { label, enabled: false }
 
-    render :: ButtonState -> H.ComponentHTML ButtonAction () m
-    render { label } = HH.button_ [ HH.text label ]
+  -- This component has no child components. When the rendered button is clicked
+  -- we will evaluate the `Click` action.
+  render :: ButtonState -> H.ComponentHTML ButtonAction () m
+  render { label, enabled } =
+    HH.button
+      [ HE.onClick \_ -> Just Click ]
+      [ HH.text $ label <> " (" <> (if enabled then "on" else "off") <> ")" ]
 
-    handleAction :: ButtonAction -> H.HalogenM ButtonState ButtonAction () output m Unit
-    handleAction = case _ of
-      Receive input ->
-        H.modify_ _ { label = input.label }
+  handleAction
+    :: ButtonAction
+    -> H.HalogenM ButtonState ButtonAction () ButtonOutput m Unit
+  handleAction = case _ of
+    -- When we receive new input we update our `label` field in state.
+    Receive input ->
+      H.modify_ _ { label = input.label }
+
+    -- When the button is clicked we update our `enabled` field in state, and
+    -- we notify our parent component that the `Clicked` event happened.
+    Click -> do
+      H.modify_ \state -> state { enabled = not state.enabled }
+      H.raise Clicked
+
+  handleQuery
+    :: forall a
+     . ButtonQuery a
+    -> H.HalogenM ButtonState ButtonAction () ButtonOutput m (Maybe a)
+  handleQuery = case _ of
+    -- When we receive a the tell-style `SetEnabled` query with a boolean, we
+    -- set that value in state.
+    SetEnabled value next -> do
+      H.modify_ _ { enabled = value }
+      pure (Just next)
+
+    -- When we receive a the request-style `GetEnabled` query, which requires
+    -- a boolean result, we get a boolean from our state and reply with it.
+    GetEnabled reply -> do
+      enabled <- H.gets _.enabled
+      pure (Just (reply enabled))
